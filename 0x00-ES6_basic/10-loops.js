d2db7c668ce91d655615acb4ea1ff3106933a3d0
scaarifintl <scaarifintl@gmail.com>
2023-04-19 18:31:39 +0200

    implement user's patient (including multiple accounts) experience: list records for a patient account, provide for switching patient accounts, hide or unhide records, delete, update and create records. NOTE: reading data from form (for update and creation undone -> do next)

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of Object.keys(array)) {
     const value = array[idx];
-    // console.log(idx, value)
     array[idx] = appendString + value;
   }
   return array;
