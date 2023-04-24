94daeedd111b66215e9b5cf8d8a85d7c0e9d1352
scaarifintl <scaarifintl@gmail.com>
2023-04-24 23:16:10 +0300

    fix 4 - create a variable instead of assigning/mutating student param (in arrow fn) -> seems to cause lint errors"

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,11 +3,12 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   if (!Array.isArray(students) || typeof city !== 'string') { return 0; }
   return students.filter((student) => student.location === city).map((student) => {
     const sGrade = newGrades.filter((grade) => grade.studentId === student.id);
+    const studentObj = { ...student }; // assigning/mutating param seems to cause lint errors
     if (sGrade.length) {
-      student.grade = sGrade[0].grade;
+      studentObj.grade = sGrade[0].grade;
     } else {
-      student.grade = 'N/A';
+      studentObj.grade = 'N/A';
     }
-    return student;
+    return studentObj;
   });
 }
